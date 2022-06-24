d2902953d1e18c5e3200d677bd4a09495ef9ca6c
labor-day <shaffer.scott@yahoo.com>
2022-06-23 18:00:09 -0600

    completed

--- classes/student.js
+++ classes/student.js
@@ -2,6 +2,29 @@ const Person = require('./person');
 
 // Your code here
 
+class Student extends Person {
+
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2) {
+    if (student1.GPA === student2.GPA) {
+      return `Both students have the same GPA.`;
+    } else if (student1.GPA > student2.GPA) {
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+    } else {
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+    }
+  }
+}
+
+let bob = new Student("bob", "Jones", 10, "math", 4.0);
+
+console.log(bob);
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,21 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce(
+      (total, teacher) => total + teacher.yearsOfExperience, 0
+    );
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
